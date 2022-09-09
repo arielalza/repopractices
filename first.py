937f51503beb2d5fbb0137584fe19791dbf087a2
arielalza <arieljay7u7@gmail.com>
2022-09-09 15:20:45 -0300

    Practices in Python3

---
+++ first.py
@@ -0,0 +1,31 @@
+###Personal note reminder: The double "##" means that part works
+
+#Firts steps on python =)
+
+##print( 7%(5//2) )
+##print( (3**2)//2 )
+ 
+
+#Part 1: Core python, basic concepts, strings & variables, functions & modules
+#regular expressions, control structures, and more.
+
+#Choice $1.000.000 or 0.01 doubled every day for 30 days.
+#Task: Write a program to calculate the result amount from the doubling penny.
+##print(0.01*(2**30))
+#Thats result in 10737418.24 (first attemp and correct)
+
+#For some text we need use the backslash \. For example to use single quote
+#in texts. 
+#This doesn't works. The output results in syntax error because we don't use \
+#print('Hi I'm Ariel')
+#This print works, the output is 'Hi I'm Ariel'
+##print('Hi I\'m Ariel')
+
+#We can use the \ for other things (like in bash) for new tabs = \t 
+#for new lines =  \n 
+#and other uses.And we can use triple quote that automatically add new
+#lines for strings example:
+##print("""there are 
+#multiple
+#lines""")
+
